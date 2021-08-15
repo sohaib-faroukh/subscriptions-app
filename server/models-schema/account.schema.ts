b871a0b0c598c14f583141de31f490071141a6c1
Sohaib GitHub <so.872562@gmail.com>
2021-08-16 00:50:42 +0300

    delete entities

---
+++ server/models-schema/account.schema.ts
@@ -0,0 +1,7 @@
+import { Account } from 'models/account';
+import { Schema, Document } from 'mongoose';
+
+class AccountDocument extends Document { }
+
+// Schema
+const UserSchema = new Schema( {} );
