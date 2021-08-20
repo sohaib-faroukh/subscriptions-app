e070e3d911b5df9d89bbf8625a62b364f9216360
Sohaib GitHub <so.872562@gmail.com>
2021-08-20 14:09:24 +0300

    login & abstract services pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from './core/services/authentication.service';
-
+enum ComponentStatus {
+	initial = 0,
+	starting = 1,
+	loading = 2,
+	done = 3,
+}
 @Component( {
 	selector: 'app-root',
 	templateUrl: './app.component.html',
-	styleUrls: [ './app.component.scss' ]
+	styleUrls: [ './app.component.scss' ],
 } )
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+	loading: ComponentStatus = ComponentStatus.initial;
 	constructor ( public auth: AuthenticationService ) { }
+
+	async ngOnInit (): Promise<void> {
+		this.loading = ComponentStatus.starting;
+		await this.auth.checkIfLoggedIn();
+		this.loading = ComponentStatus.done;
+	}
 }
--- src/app/core/services/authentication.service.ts
+++ src/app/core/services/authentication.service.ts
@@ -5,13 +5,14 @@ import { BehaviorSubject, Observable, of } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { BaseCrudService } from '../models/base-crud-service';
 import { HttpSearchOptions } from '../models/http-search-options';
-import { saveToStorage } from '../utils/local-storage.util';
+import { getItemFromStorage, saveToStorage } from '../utils/local-storage.util';
 
 @Injectable()
 export class AuthenticationService extends BaseCrudService<IAccount, HttpSearchOptions> {
 
 	// apiUrl = 'api/accounts';
 	tokenStorageKey = 'app_jwt_token';
+	private _isLoggedIn$ = new BehaviorSubject<boolean>( false );
 	auth$ = new BehaviorSubject<string>( '' );
 
 
@@ -22,8 +23,32 @@ export class AuthenticationService extends BaseCrudService<IAccount, HttpSearchO
 		this.apiUrl = 'api/accounts';
 		this.counterOfUsage++;
 	}
-	public log = () => {
-		return this.counterOfUsage?.toString();
+
+	get isLoggedIn$ (): Observable<boolean> {
+		return this._isLoggedIn$.asObservable();
+	}
+
+	/**
+	 * 1. this check is happened once on each time on the opening of the application
+	 * 2. this check should save the return status in the isLoggedIn attribute as the following:
+	 * *  `true` in case success request with success code `200`
+	 * *  `false` in case failed request
+	 */
+	public checkIfLoggedIn = async (): Promise<boolean> => {
+		let result = false;
+		try {
+			const token = getItemFromStorage( this.tokenStorageKey );
+			if ( token ) {
+				result = ( await this.http.get<boolean>( `${ this.apiUrl }/is-auth` ).toPromise() ) || false;
+			}
+		} catch ( error ) {
+			result = false;
+			console.error( '**** Not logged-in' );
+			console.error( error );
+		} finally {
+			this._isLoggedIn$.next( result );
+		}
+		return result;
 	}
 	public login = ( account: IAccount ): Observable<any> => {
 		// TODO: implement this method
