55d66798d6d9afb0f6f9cf9482cb1286b2b626cd
Sohaib GitHub <so.872562@gmail.com>
2021-09-04 00:42:23 +0300

    remove directive testing file

--- src/app/shared/loading-button/directives/loading-button.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LoadingButtonDirective } from './loading-button.directive';
-
-describe('LoadingButtonDirective', () => {
-  it('should create an instance', () => {
-    const directive = new LoadingButtonDirective();
-    expect(directive).toBeTruthy();
-  });
-});
